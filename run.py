a907fc6aa3b2a294b2621dd99c398bc86de7fd9a
David Ren <d.david.ren.r@gmail.com>
2026-02-09 00:51:41 -0500

    Added analyzer program

---
+++ run.py
@@ -0,0 +1,12 @@
+﻿import os
+import sys
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+SRC = os.path.join(ROOT, "src")
+if SRC not in sys.path:
+    sys.path.insert(0, SRC)
+
+from asphalt_cli.analyze import analyze
+
+if __name__ == "__main__":
+    analyze()
